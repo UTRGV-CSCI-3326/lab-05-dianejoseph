7280b7a1646c214d7c7bf57c835b6a573eb7bb1c
Diane Joseph <104699671+dianejoseph@users.noreply.github.com>
2025-02-02 04:32:07 +0000

    first commit

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class Input{
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.print("Enter your weight (to the nearest pound): ");
+        int weight = scanner.nextInt();
+        System.out.print("Are you a smoker (true or false)? ");
+        boolean smoker = scanner.nextBoolean();
+        scanner.close();
+
+        System.out.print("\n" + name + "\n");
+        System.out.print(age + "\n");
+        System.out.print(weight + "\n");
+        System.out.print(smoker + "\n"); 
+    }
+}
